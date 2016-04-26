1d258b2b22519dbe26f178bfbba674439e4b7785
NoahVlncrt <noah0000games@gmail.com>
2016-04-26 16:48:26 -0400

    removed dependencies and add post creation, still very rough

--- client/pages/createPost/createPost.js
+++ client/pages/createPost/createPost.js
@@ -1,14 +1,33 @@
 Template.createPostPage.onRendered(function(){
   $('select').material_select()
 })
-Template.createPostPage.events({
-  '.select click': function() {
-    $('select').material_select()
-  }
-})
+
 Template.createPostPage.helpers({
-  channels: function() {
+  createdChannels: function() {
     currentUser = Meteor.userId();
     return Channels.find({createdBy: currentUser}, {sort:{createdAt: -1}});
+  },
+  followingChannels: function(){
+    currentUser = Meteor.userId()
+    return Channels.find({followers: {$in: currentUser}})
+  }
+})
+
+Template.createPostPage.events({
+  'submit .createForm': function(){
+    event.preventDefault();
+    title = $("#postTitle").val()
+    content = $("#postContent").val()
+    parentChannel = $("#channel").val()
+    
+    Data = {
+      title: title,
+      content: content,
+      parentChannel: parentChannel,
+      parentSection: "Community",
+      postType: "Community"
+    }
+    
+    Posts.insert(Data, {validate:true})
   }
 })
--- client/templates/floatingFab/floatingFab.js
+++
@@ -1,9 +0,0 @@
-Template.floatingFab.helpers({
-  routeCheck: function() {
-   if(FlowRouter.getRouteName() === "mainPage"){
-     return "main"
-   }else {
-     return "channel"
-   } 
-  }
-});
