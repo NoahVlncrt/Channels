2d60bc83901d47a8b6cfde1f81eeef0b4d6bb8d5
NoahVlncrt <noah0000games@gmail.com>
2016-04-29 19:38:15 -0400

    few bug fixes here and there!

--- client/pages/channel/channel.js
+++ client/pages/channel/channel.js
@@ -14,20 +14,25 @@ Template.channelPage.helpers({
     currentShortName = FlowRouter.getParam("shortName");
     return Channels.findOne({shortName: currentShortName});
   },
-  followingCheck: function() {
+  isFollowing: function() {
     currentShortName = FlowRouter.getParam("shortName");
     Data = Channels.findOne({shortName: currentShortName});
-    return Data.followers.includes(Meteor.userId()) || Data.createdBy === Meteor.userId();
+    return Data.followers.includes(Meteor.userId()) === true;
+  },
+  isOwner: function(){
+    currentShortName = FlowRouter.getParam("shortName");
+    Data = Channels.findOne({shortName: currentShortName});
+    return Data.createdBy === Meteor.userId();
   }
 });
 Template.channelPage.events({
   'click .follow': function() {
-    currentId = FlowRouter.getParam("id");
-    Data = Channels.findOne({_id: currentId});
+    currentShortName = FlowRouter.getParam("shortName");
+    Data = Channels.findOne({shortName: currentShortName});
     if(Data.followers.includes(Meteor.userId()) === true) {
       console.log("seems as if you are already following this channel. . .")
     } else {
-      Channels.update({_id: currentId}, {$push: {"followers": Meteor.userId()}});
+      Channels.update({_id: Data._id}, {$push: {"followers": Meteor.userId()}});
       console.log("thank you for following!")
     }
   },
